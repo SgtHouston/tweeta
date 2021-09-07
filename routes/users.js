d957415e7c9697e61f1ff77cdd14c5338b8e1d4f
Chris Houston <chrisbcoding247@gmail.com>
2021-09-07 12:37:28 -0400

    added user register

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,58 @@
 var express = require('express');
 var router = express.Router();
+// require db models
+const db = require('../models')
+const bcrypt = require('bcrypt')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Register Route
+router.post('/register', function(req, res, next) {
+  // take username, password
+  if (!req.body.username || ! req.body.password){
+    res.status(400).json({
+      error: 'please include username and password'
+    })
+    return
+  }
+  // create a new user (returns a promise)
+  db.User.findOne({
+    where: {
+      username: req.body.username
+    }
+  })// check if username is taken
+    .then((user) => {
+      if(user) {
+        res.status(400).json({
+          error: 'username already in use'
+        })
+        return 
+      }
+    // bycrypt hash password
+    bcrypt.hash(req.body.password, 10)
+      .then((hash) => {
+        // store in db
+        db.User.create({
+          username: req.body.username,
+          password: hash
+        })
+          .then((user) => {
+            // respond with success
+            res.status(201).json({
+              success: 'User created'
+            })
+
+          })
+      })
+    
+  
+    })
+    
 });
+
+
+// Login Route
+// Register Route
+// Logout route 
+
+
 
 module.exports = router;
